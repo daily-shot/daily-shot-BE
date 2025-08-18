aa3860ebfed61a9c363879fab1820486bcee8b7e
moonsunmeane <anstjsals4078@gmail.com>
2025-08-18 15:15:24 +0900

    first commit

---
+++ src/main/java/dailyshot/dailyshot_back/DailyshotBackApplication.java
@@ -0,0 +1,14 @@
+package dailyshot.dailyshot_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyshotBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyshotBackApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/dailyshot/dailyshot_back/DailyshotBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dailyshot.dailyshot_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyshotBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
